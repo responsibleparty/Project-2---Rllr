2b233d1b53455381811244232ac2e5dc96a3352f
Hai Duong <caubenondo@gmail.com>
2022-07-12 01:17:00 -0700

    the card are sortable and capture the formation

--- public/js/frontendhelper.js
+++ public/js/frontendhelper.js
@@ -5,7 +5,7 @@ const capitalize = (str) => {
         .join(" ");
 };
 
-const one_theme = (type)=>{
+const one_theme = (type) => {
     const typeColor = {
         bug: "#26de81",
         dragon: "#ffeaa7",
@@ -24,8 +24,8 @@ const one_theme = (type)=>{
         rock: "#2d3436",
         water: "#0190FF",
         steel: "#7a7f80",
-        dark:'#383838'
-      };
+        dark: "#383838",
+    };
     return typeColor[type];
 };
 
@@ -33,19 +33,30 @@ const buildMyCard = (poke, index) => {
     let templateHtml = `
     
     <div class="poke-card"
-    style="background: radial-gradient(circle at 50% 0%, ${one_theme(poke.types[0])} 36%, rgb(255, 255, 255) 36%);">
+    style="background: radial-gradient(circle at 50% 0%, ${one_theme(
+        poke.types[0]
+    )} 36%, rgb(255, 255, 255) 36%);">
     <p class='handle'><i class="fa-2x fa-solid fa-grip"></i></p>
     <p class="hp">
         <span>ID</span>
         ${poke.id}
         
     </p>
-    <img class='animate__animated animate__infinite animate__slower animate__pulse'
-        src="/images/pokemons/${poke.api_id}.svg">
+    <a href='/pokedetail/${poke.api_id}' target='_blank'>
+    <img class='animate__animated animate__infinite animate__slower animate__pulse' src="/images/pokemons/${
+        poke.api_id
+    }.svg">
+    </a>
+    
     <h2 class="poke-name">${capitalize(poke.name)}</h2>
     <div class="types">
        
-        ${poke.types.map(type=>`<span style="background-color:${one_theme(type)};">${capitalize(type)}</span>`)}
+        ${poke.types.map(
+            (type) =>
+                `<span style="background-color:${one_theme(
+                    type
+                )};">${capitalize(type)}</span>`
+        )}
     </div>
     <div class="stats">
         <div>
@@ -53,7 +64,9 @@ const buildMyCard = (poke, index) => {
             <p>HP</p>
         </div>
         <div>
-            <h3>${poke.stat_att} <i class="fa-solid fa-location-crosshairs"></i></h3>
+            <h3>${
+                poke.stat_att
+            } <i class="fa-solid fa-location-crosshairs"></i></h3>
             <p>ATT</p>
         </div>
         <div>
@@ -68,17 +81,21 @@ const buildMyCard = (poke, index) => {
             <p>SPD</p>
         </div>
         <div>
-            <h3>${poke.stat_special_att} <i class="fa-solid fa-skull-crossbones"></i></h3>
+            <h3>${
+                poke.stat_special_att
+            } <i class="fa-solid fa-skull-crossbones"></i></h3>
             <p>Sp. ATT</p>
         </div>
         <div>
-            <h3>${poke.stat_special_def} <i class="fa-solid fa-shield-virus"></i></h3>
+            <h3>${
+                poke.stat_special_def
+            } <i class="fa-solid fa-shield-virus"></i></h3>
             <p>Sp. DEF</p>
         </div>
     </div>
     <div class="abilities">
         <p> <span class="h6">Abilities: </span>
-            ${poke.abilities.map(abi=>` <span class='pill'>${abi}</span>`)}
+            ${poke.abilities.map((abi) => ` <span class='pill'>${abi}</span>`)}
         </p>
 
     </div>
