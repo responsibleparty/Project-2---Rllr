ba9d8d6b3f939ca275277ed19e4d37a5acac9942
Hai Duong <caubenondo@gmail.com>
2022-07-11 21:06:40 -0700

    fix sematic css and html for the card, dont use id for repeatable element

--- controllers/api/pokemonRoutes.js
+++ controllers/api/pokemonRoutes.js
@@ -49,5 +49,30 @@ router.get('/:query', async (req, res) => {
         res.status(500).json(error)
     }
 })
+router.get('/', async (req,res)=>{
+    try {
+        const pokeData = await Pokedex.findAll();
+        const pokedex = pokeData.map((p) => {
+            const pokemon = p.get({
+                plain: true
+            })
+         
+            const newPokemon = {
+                ...pokemon,
+                abilities: JSON.parse(pokemon.abilities),
+                types: JSON.parse(pokemon.types),
+                
+            }
+            return newPokemon
+        })
 
+        res.status(200).json(pokedex)
+        // res.render('pokesort', {
+        //     pokedex,
+        //     logged_in:req.session.logged_in
+        // })
+    } catch (error) {
+        res.status(500).json(error)
+    }
+})
 module.exports = router;
